3534d496c94d996521f952a4ae822d16631578c8
Romer J <romer.jos.02@gmail.com>
2026-03-27 01:33:39 -0500

    commit pruebas

---
+++ src/main/java/com/commit/PruebasCommitApplication.java
@@ -0,0 +1,13 @@
+package com.commit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasCommitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasCommitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commit/PruebasCommitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasCommitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
