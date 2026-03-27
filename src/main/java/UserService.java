c8891375c11cbcf33e0963d2c369f9aa0b68f236
Romer J <romer.jos.02@gmail.com>
2026-03-27 01:46:13 -0500

    feat: implementar método

---
+++ src/main/java/UserService.java
@@ -0,0 +1,10 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UserService {
+
+    public List<String> listarUsuarios() {
+        return Arrays.asList("Juan", "Maria", "Pedro");
+    }
+}
